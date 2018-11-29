ff9acb30071cf9904072ea0f48b38fa4f60cb5d8
Michaela <43633560+evelyn9191@users.noreply.github.com>
2018-11-29 11:00:39 +0100

    Version 1.0 (working)

--- newsletter_creator.py
+++ newsletter_creator.py
@@ -1,9 +1,16 @@
+# Newsletter creator takes tags containing links to articles
+# from homepage tyinternety.cz and sends the tags as html text
+# within an email to receiver's email address. Script is then
+# run on personal computer once a week via Windows Task Scheduler.
+# The script is working, but the email body looks nasty, therefore:
+# TODO: fix the look of the email
+
 import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import email_data # TODO: Will this usage work?
+import email_data
 
 def check_articles():
     """Parse web for html tag.
@@ -29,22 +36,22 @@ def create_email(check_articles, email_data):
     :param str check_articles
     :return: email with str check_articles
     """
-    fromaddr = sender
-    toaddr = receiver
+    fromaddr = email_data.sender
+    toaddr = email_data.receiver
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = "Nova zprava z tyinternety.cz"
 
-    body = articles
-    msg.attach(MIMEText(body, "plain"))
+    body = "Toto jsou nejnovejsi clanky z tyinternety.cz:\n {}".format(check_articles())
+    msg.attach(MIMEText(body, "plain", "utf-8"))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.connect('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(fromaddr, openkeyword)
+    server.login(fromaddr, email_data.openkeyword)
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
@@ -52,4 +59,4 @@ def create_email(check_articles, email_data):
 
 if __name__ == "__main__":
     articles_list = check_articles()
-    email = create_email(check_articles, email_data)
+    send_email = create_email(check_articles, email_data)
