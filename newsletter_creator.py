b15f806baefb2fd34908dd7e5a2299433e2bfa40
evelyn9191 <ockova.michaela@gmail.com>
2018-12-01 16:02:30 +0100

    Fix email format and comments

--- newsletter_creator.py
+++ newsletter_creator.py
@@ -2,8 +2,12 @@
 # from homepage tyinternety.cz and sends the tags as html text
 # within an email to receiver's email address. Script is then
 # run on personal computer once a week via Windows Task Scheduler.
-# The script is working, but the email body looks nasty, therefore:
-# TODO: fix the look of the email
+#
+# There has to be module :email_data with email account access info.
+# The module has to include variables:
+# * openkeyword: password for gmail account
+# * sender: sender email address
+# * receiver: receiver email address
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,9 +16,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import email_data
 
+
 def check_articles():
     """Parse web for html tag.
+
     Find class post-thumbnail used for article thumbnails.
+
     :return: div including links to articles
     """
     url = "https://tyinternety.cz/"
@@ -25,25 +32,28 @@ def check_articles():
     html = response.text
 
     soup = BeautifulSoup(html, 'lxml')
-    articles = soup.find_all(class_="post-thumbnail")   # How can I make the upper link generate only
-                                                    # the a href links within the class
-                                                    # and only within the a href, i.e. not include the img tag?
+    articles = soup.find_all(class_="entry-title")
+
     return articles
+
 
 def create_email(check_articles, email_data):
     """Send email with links to articles.
+
     Put class with articles into email as HTML language and send it.
-    :param str check_articles
-    :return: email with str check_articles
+
+    :param [str] check_articles: List with extracted article titles and links.
+    :param module email_data: Module with variables with email access info.
     """
     fromaddr = email_data.sender
     toaddr = email_data.receiver
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "Nova zprava z tyinternety.cz"
+    msg['Subject'] = "Nove clanky z tyinternety.cz"
 
-    body = "Toto jsou nejnovejsi clanky z tyinternety.cz:\n {}".format(check_articles())
+    articles = "\n ".join(check_articles)
+    body = "Toto jsou nejnovejsi clanky z tyinternety.cz:\n {}".format(articles)
     msg.attach(MIMEText(body, "plain", "utf-8"))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -57,6 +67,7 @@ def create_email(check_articles, email_data):
     server.quit()
     print("Send mail success")
 
+
 if __name__ == "__main__":
     articles_list = check_articles()
-    send_email = create_email(check_articles, email_data)
+    send_email = create_email(check_articles=articles_list, email_data=email_data)
