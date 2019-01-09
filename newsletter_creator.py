2dff0af0e73ea3c4acb7adc028f6780b7ffa0af0
Michaela <43633560+evelyn9191@users.noreply.github.com>
2019-01-09 22:54:37 +0100

    Version 1.0 (finished, fully working)

--- newsletter_creator.py
+++ newsletter_creator.py
@@ -1,7 +1,7 @@
-# Newsletter creator takes tags containing links to articles
-# from homepage tyinternety.cz and sends the tags as html text
-# within an email to receiver's email address. Script is then
-# run on personal computer once a week via Windows Task Scheduler.
+# Newsletter creator extracts tags containing article titles and their urls
+# from homepage tyinternety.cz, cleans them from html and sends those
+# by email to receiver's email address. Script is then run on personal computer
+# once a week via Windows Task Scheduler.
 #
 # There has to be module :email_data with email account access info.
 # The module has to include variables:
@@ -10,19 +10,21 @@
 # * receiver: receiver email address
 
 import requests
+
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 import email_data
 
 
-def check_articles():
-    """Parse web for html tag.
+def get_articles():
+    """Parse web for html tags.
 
-    Find class post-thumbnail used for article thumbnails.
+    Find class entry-title used in article titles tags.
 
-    :return: div including links to articles
+    :return: [dict] with article titles as keys and article urls as values
     """
     url = "https://tyinternety.cz/"
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
@@ -33,28 +35,40 @@ def check_articles():
 
     soup = BeautifulSoup(html, 'lxml')
     articles = soup.find_all(class_="entry-title")
+    article_soup = BeautifulSoup(str(articles), 'lxml')
+    urls = str(article_soup.find_all('a'))
+    urls_list = urls.split('</a>, ')
+    articles_dict = {}
+    for value in urls_list:
+        head, sep, tail = value.partition('\">')
+        tag, part, link = head.partition('href=\"')
+        if '</a>]' in tail:
+            tail = tail.replace('</a>]', '')
+        if 'bookmark' in link:
+            url, separator, bookmark = link.partition('\"')
+            articles_dict[tail] = url
+        else:
+            articles_dict[tail] = link
+    return articles_dict
 
-    return articles
 
+def create_email(get_articles, email_data):
+    """Send email with article titles and their urls.
 
-def create_email(check_articles, email_data):
-    """Send email with links to articles.
+    Nicely format article titles and urls, attach them to email body and send the email.
 
-    Put class with articles into email as HTML language and send it.
-
-    :param [str] check_articles: List with extracted article titles and links.
+    :param [dict] get_articles: Dictionary with extracted article titles and urls.
     :param module email_data: Module with variables with email access info.
     """
+    articles_dict = get_articles()
     fromaddr = email_data.sender
     toaddr = email_data.receiver
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "Nove clanky z tyinternety.cz"
-
-    # was not working with: articles = "\n ".join(check_articles) and then in body with .format(check_articles())
-    body = "Toto jsou nejnovejsi clanky z tyinternety.cz:\n {}".format(articles)
-    msg.attach(MIMEText(body, "plain", "utf-8"))
+    msg['Subject'] = 'Nové články na tyinternety.cz'
+    body = MIMEText(('\n\n'.join('{}\n{}'.format(key, value) for key, value in articles_dict.items())), 'plain')
+    msg.attach(body)
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.connect('smtp.gmail.com', 587)
@@ -65,9 +79,9 @@ def create_email(check_articles, email_data):
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
-    print("Send mail success")
+    print("Email successfully sent!")
 
 
 if __name__ == "__main__":
-    articles_list = check_articles()
-    send_email = create_email(check_articles=articles_list, email_data=email_data)
+    get_articles()
+    create_email(get_articles, email_data)
